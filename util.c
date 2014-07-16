a5747180a27930711534645d8458ab198b5f7af9
Nate Woolls <nwoolls@gmail.com>
2014-07-15 22:32:12 -0400

    util: Work around broken Scrypt pools automatically using realistic pdiff assumptions
    Reasoning: we need to add another condition to the existing "if (opt_scrypt)" work-around as ASICs approach pdiff 1.
    If we are to add an additional check for pdiff > N, it makes sense to use a realistic value to adjust all Scrypt pool diffs.

--- util.c
+++ util.c
@@ -2460,22 +2460,29 @@ static bool parse_diff(struct pool *pool, json_t *val)
 
 	if ((int64_t)diff != diff)
 	{
-		// Always assume fractional values are proper bdiff per specification
+		// Assume fractional values are proper bdiff per specification
+		// Allow integers to be interpreted as pdiff, since some the difference is trivial and some pools see it this way
 		diff = bdiff_to_pdiff(diff);
-	}
-	else
-	{
-		// Integer; allow it to be interpreted as pdiff, since some the difference is trivial and some pools see it this way
-		if (opt_scrypt)
-		{
-			// Some scrypt pools multiply difficulty by 0x10000; since diff 1 is pretty difficult for scrypt right now, this is a safe assumption (otherwise they would be using a fractional value)
-			diff /= 0x10000;
-		}
 	}
 	
 	if ((!opt_scrypt) && diff < 1 && diff > 0.999)
 		diff = 1;
 	
+#ifdef USE_SCRYPT
+	// Broken Scrypt pools multiply difficulty by 0x10000
+	const double broken_scrypt_diff_multiplier = 0x10000;
+
+	/* 7/12/2014: P2Pool code was fixed: https://github.com/forrestv/p2pool/pull/210
+	   7/15/2014: Popular pools unfixed: wemineltc, dogehouse, p2pool.org
+                  Cannot find a broken Scrypt pool that will dispense diff 64 or lower */
+	const double minimum_broken_scrypt_diff = 64;
+
+	// Ideally pools will fix their implementation and we can remove this
+	// This should carry us until folks are hashing Scrypt with > ~256 Gh/s (diff 1 = 4 Gh/s)
+	if (opt_scrypt && (diff > minimum_broken_scrypt_diff))
+		diff /= broken_scrypt_diff_multiplier;
+#endif
+
 	cg_wlock(&pool->data_lock);
 	set_target_to_pdiff(pool->swork.target, diff);
 	cg_wunlock(&pool->data_lock);
